9a53ffa36d3e57b74ca3f1dc9908013c9f2aa040
fahmanfaisal <fahmanchy@gmail.com>
2021-01-06 19:00:08 +0600

    shorting arrays

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+
+<html>
+<head>
+   <title> Lear PHP </title>
+<style>
+       .header {width=90%; margin:0}
+
+</style>
+</head>
+
+<body>
+<section>
+      <div class="header">
+        <h1><?php echo Learn PHP?></h1>
+
+      </div>
+</section>
+
+<section>
+
+      <div class="body">
+         <h1><?php echo "---Code Here---"?></h1>
+		 <hr>
+		 
+
+       </div>
+	   
+</section>
+<section>
+         <div class="footer">
+<h1><?php
+echo Footer?></h1>
+
+</div>
+</section>
+
+</body>
+</html>
