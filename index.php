af9fafa0878e81942fc08d0d06ebc0b860b03a02
fahmanfaisal <fahmanchy@gmail.com>
2021-01-09 17:23:04 +0600

    Shortig Array & Variables Scope

--- index.php
+++ index.php
@@ -3,36 +3,91 @@
 <html>
 <head>
    <title> Lear PHP </title>
-<style>
-       .header {width=90%; margin:0}
-
-</style>
+   <style type="text/css">
+       
+.hf{
+        width:900px;
+        margin:0 auto;
+        padding: 20px 10px; 
+        background:#1e73be; 
+        text-align: center; 
+        font-weight: bold;
+    }    
+.content{
+        width:900px; 
+        margin:0 auto; 
+        height:600px;
+        background:#e4edf8; 
+        padding: 20px 10px; 
+        text-align:center;
+    }
+   </style>
 </head>
 
 <body>
 <section>
-      <div class="header">
-        <h1><?php echo Learn PHP?></h1>
-
-      </div>
+    <div class="hf">
+	<h2>Code Here With Proper Syntex</h2>
+        <?php echo "";
+		?>
+		<hr>	
+    </div>
 </section>
 
+
 <section>
+    <div class="content">
+	    <h4>Shorting Arrays</h4>	 
+        <div>
+        <?php
+        
+            $name= array ("jhon", "Rohan" ,"ElderRohan" ,"ElderJhon");
+            asort($name);
+            $length= count($name);         //shorting arrays in dynamic way!
+            for ($i=0; $i <$length ; $i++) { 
+                echo $name[$i];
+                echo "</br>";
+            }
+        ?>
+        </div>
+    
+        <h4>PHP Varialble Scope</h4>     
+        <div>
+        <?php
+              $x=15;
+            function test1(){
+                global $x;
+                $a=5;
+                echo $a;
+                echo "</br>";
+                echo "Access form Global vaialble $x";
 
-      <div class="body">
-         <h1><?php echo "---Code Here---"?></h1>
-		 <hr>
-		 
+            }
+            function test2(){
+                global $x;
+                $b=10;
+                echo "</br>";
+                echo $b;
+                echo "</br>";
+                echo "Access form Global vaialble $x";
 
-       </div>
-	   
+            }
+            test1();
+            test2();
+        ?>
+        </div>
+
+    </div>    
 </section>
-<section>
-         <div class="footer">
-<h1><?php
-echo Footer?></h1>
 
-</div>
+
+<section>
+    <div class="hf">
+        <?php
+        echo "Footer";
+        ?>
+    </div> 
+
 </section>
 
 </body>
