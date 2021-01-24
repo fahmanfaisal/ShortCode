a907d98002a199e89a2c686bb8cec7ccff12bcaa
fahmanfaisal <fahmanchy@gmail.com>
2021-01-25 01:49:51 +0600

    Gobal Variables & Super Global Variables

--- index.php
+++ index.php
@@ -21,7 +21,11 @@
         padding: 20px 10px; 
         text-align:center;
     }
+
+
    </style>
+}
+}
 </head>
 
 <body>
@@ -55,8 +59,8 @@
         <div>
         <?php
               $x=15;
-            function test1(){
-                global $x;
+            function test1(){              
+                global $x;              //global variable
                 $a=5;
                 echo $a;
                 echo "</br>";
@@ -76,12 +80,72 @@
             test2();
         ?>
         </div>
+ 
+         <hr>
+        <h4>Gobal Variables</h4></hr>
+    <P> $GLOBALS
+        $_SERVER
+        $_REQUEST
+        $_POST
+        $_GET
+        $_FILE
+        $_ENV
+        $_COOKIE 
+        $_SESSION </P> 
 
-    </div>    
+        <div>
+        <?php 
+            $x= 10;
+            $y= 20;
+
+            function sum(){
+                $GLOBALS ['z'] =  $GLOBALS ['x'] + $GLOBALS ['y'];
+            }
+            sum();
+            echo $z;
+            
+            echo "</br>";
+        ?>
+        <?php
+            
+            echo $_SERVER['PHP_SELF'];
+        ?>
+        </div>
+
+        <hr>
+        <h4>Php Super Globals [$_REQUEST & $_POST]</h4>
+        <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
+        User Name: <input type="text" name="name">
+        <input type="submit" value="Submit">
+        </form>
+    
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $name= $_REQUEST['name'];
+            if (empty($name)) {
+                echo "Username Field Must Not Be Empty !!";
+            } else{
+                echo "You Have Submited: ".$name;
+            }    
+        }
+        ?>
+    <hr>
+    <h4>SuperGlobals Variable [$_GET]</h4>
+    <a href="test.php?msg=What Time Is It?&txt=It's Fun Time !!">Sent Data By Super Variables: $_GET</a>
+
+    <?php 
+
+    ?>
+
+
+
+
+
+    </div>   
 </section>
+ 
 
-
-<section>
+<section style="margin-top: 50px;">
     <div class="hf">
         <?php
         echo "Footer";
---
+++ test.php
@@ -0,0 +1,10 @@
+<?php 
+$message = $_GET['msg'];
+$message2 = $_GET['txt'];
+
+echo "A Boy Asked :".$message;
+echo "</br";
+echo " Girl Replied :".$message2;
+
+
+?>
